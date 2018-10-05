3259d6402686e0c3569b11553916b432a42c4b1f
Rodrigo235 <r.rodrigomoliveira@gmail.com>
2018-10-05 14:16:47 -0300

    mudado a estrutura do projeto para mais organização

---
+++ Projeto/model/Estoque.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+  * 
+  */
+ class Estoque {
+
+ 	private $arrayProdutos;
+ 	private $recebido = 0;
+ 	private $gasto = 0;
+ 	
+ 	function __construct($produtos) {
+ 		$this->arrayProdutos = $produtos;
+ 		$this->recebido = 0
+ 		for ($i=0; $i < sizeof($produtos); $i++) { 
+ 			$this->gasto = $this->gasto + $produtos[i]->getValor();
+ 		}
+ 	}
+
+ 	public function getArrayProdutos () {
+ 		return $this->produtos;
+ 	}
+
+ 	public function setArrayProdutos ($produtos) {
+ 		$this->produtos = $produtos;
+ 	}
+
+ 	public function removerProduto ($produto) {
+ 		$this->setProdutos()
+ 	}
+
+ } 
+
+ ?>
